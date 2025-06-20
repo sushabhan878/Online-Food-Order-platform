bc54d034cb8593b935716f4fbc318b0ab22606ac
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-20 11:46:45 +0530

    Initialized the client folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className='button'>Lets build out food ordring website using TS</Button>
+    </>
+  )
+}
+
+export default App
